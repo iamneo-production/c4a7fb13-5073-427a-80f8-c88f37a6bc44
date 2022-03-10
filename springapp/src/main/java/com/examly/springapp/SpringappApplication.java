66492cda4acc09b7e33bdb5cda35f45b02888cc4
hritik-h <hritik.bnp@gmail.com>
2022-03-10 17:13:24 +0530

    test commit 2

--- springapp/src/main/java/com/examly/springapp/SpringappApplication.java
+++ springapp/src/main/java/com/examly/springapp/SpringappApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringappApplication {
-	//test commit
+	//test commit 2
 	public static void main(String[] args) {
 		SpringApplication.run(SpringappApplication.class, args);
 	}
